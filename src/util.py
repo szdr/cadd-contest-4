d9d1081c20b17e52e456e0b226e4248f60d6b449
ShogoSuzuki <s_suzuki@bi.c.titech.ac.jp>
2017-06-14 22:10:18 +0900

    [Update] smiles2feature_as_array

--- src/util.py
+++ src/util.py
@@ -21,6 +21,9 @@ def get_feature_names(nbits=2048):
 
 
 def smiles2feature(smiles, desc_names):
+    """
+    featureは辞書で返す
+    """
     mol = Chem.MolFromSmiles(smiles)
 
     # ECFP4
@@ -38,3 +41,29 @@ def smiles2feature(smiles, desc_names):
             continue
         ret[desc_name] = desc_func(mol)
     return ret
+
+
+def smiles2feature_as_array(smiles, desc_names):
+    """
+    featureはnumpy arrayで返す
+    ECFP4, descriptorsの順番
+    """
+    mol = Chem.MolFromSmiles(smiles)
+
+    # ECFP4
+    ecfp4 = Chem.GetMorganFingerprintAsBitVect(mol, radius=2, nBits=2048)
+    ecfp4_arr = np.zeros((1, ))
+    DataStructs.ConvertToNumpyArray(ecfp4, ecfp4_arr)
+
+    # Descriptor
+    # WARNING: Descriptors.descListとdesc_namesの順番は同じ
+    descriptor_arr = np.zeros(len(desc_names))
+    desc_name_set = set(desc_names)
+    ind = 0
+    for desc_name, desc_func in Descriptors.descList:
+        if desc_name not in desc_name_set:
+            continue
+        descriptor_arr[ind] = desc_func(mol)
+        ind += 1
+
+    return np.r_[ecfp4_arr, descriptor_arr]
